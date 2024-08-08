4e8232d9f4e88c69fe47b7b88013bd320b7bec9c
OgnjenJankovic <oj20190129@student.fon.bg.ac.rs>
2024-08-09 01:42:07 +0200

    Dokumentovane klase AbstractDomainObject i Administrator

--- KosarkaZajednicki/src/main/java/rs/ac/bg/fon/nprog/domain/AbstractDomainObject.java
+++ KosarkaZajednicki/src/main/java/rs/ac/bg/fon/nprog/domain/AbstractDomainObject.java
@@ -5,16 +5,84 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+
+/**
+ *Abstraktna klasa koja daje specifikaciju operacija koje su neophodne za rad sa generickim upitima prilikom komunikacije sa bazom podatada
+ *
+ *Predstavlja visi nivo apstrakcije u odnosu na sve domenske klase.
+ *
+ *Takodje, nasledjuje interfejs Serializable kako bi objekat mogao da se prenosi preko mreze
+ *
+ *@author 2000o
+ *@version 1.1.0
+ */
 public abstract class AbstractDomainObject implements Serializable{
 
+	/**
+	 * Vraca naziv tabele u bazi podataka
+	 * 
+	 * @return Naziv tabele kao String
+	 */
 	public abstract String nazivTabele();
+	
+	/**
+	 * Vraca alijas tabele iz baze podataka
+	 * 
+	 * @return Alijas tabele kao String 
+	 */
     public abstract String alijas();
+    
+    /**
+     * Kombinuje tabele na specifican nacin i vraca rezultat u formi Stringa
+     * 
+     * @return String koji predstavlja rezultat kombinacije
+     */
     public abstract String join();
+    
+    /**
+     * Vraca listu objekata koja predstavlja ucitane redove iz baze podataka nakon izvrsnja upita.
+     * Prolazi se po redovima, ucitavaju se podaci iz svakog reda baze podataka i postaljaju
+     * na vrednosti atributa objekata koji se dodaje u listu.
+     * 
+     * @param rs Objekat koji sadrzi podatke iz baze podataka nakon izvrsavanja upita.
+     * @return Lista objekata ucitanih na osnovu vrednosti u bazi podataka nakon izvrsavanja upita.
+     * @throws SQLException ako dodje do greske prilikom ucitavanja podataka iz baze
+     */
     public abstract ArrayList<AbstractDomainObject> vratiListu(ResultSet rs) throws SQLException;
+    
+    /**
+     * Vraća string koji predstavlja imena kolona koje će biti uključene u SQL INSERT upit.
+     * 
+     * @return String sa imenima kolona za INSERT upit.
+     */
     public abstract String koloneZaInsert();
+    
+    /**
+     * Vraća string koji predstavlja uslov (WHERE klauzulu) za SQL upit.
+     * 
+     * @return String koji definiše uslov za SQL upit.
+     */
     public abstract String uslov();
+    
+    /**
+     * Vraća string koji predstavlja vrednosti koje će biti uključene u SQL INSERT upit.
+     * 
+     * @return String sa vrednostima za INSERT upit.
+     */
     public abstract String vrednostiZaInsert();
+    
+    /**
+     * Vraća string koji predstavlja vrednosti i njihove odgovarajuće kolone za SQL UPDATE upit.
+     * 
+     * @return String sa vrednostima i kolonama za UPDATE upit.
+     */
     public abstract String vrednostiZaUpdate();
+    
+    /**
+     * Vraća string koji predstavlja uslov (WHERE klauzulu) za SQL SELECT upit.
+     * 
+     * @return String koji definiše uslov za SELECT upit.
+     */
     public abstract String uslovZaSelect();
 	
 	
--- KosarkaZajednicki/src/main/java/rs/ac/bg/fon/nprog/domain/Administrator.java
+++ KosarkaZajednicki/src/main/java/rs/ac/bg/fon/nprog/domain/Administrator.java
@@ -5,14 +5,49 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/**
+ * Klasa koja predstavlja administratora sistema
+ * 
+ * Ova klasa implementira AbstractDomainObject u odnosu na tabelu administrator u bazi
+ * 
+ * Administrator ima svoj ID koji je jedinstven identifikator, kao i ime, prezime, username i password
+ * 
+ * @author 2000o
+ * @version 1.1.0
+ */
 public class Administrator extends AbstractDomainObject{
 
+	/**
+	 * ID administratora kao Long
+	 */
 	private Long administratorID;
+	/**
+	 * Ime administratora kao String 
+	 */
     private String ime;
+    /**
+     * Prezime administratora kao String
+     */
     private String prezime;
+    /**
+     * Username administratora kao String 
+     */
     private String username;
+    /**
+     * Password administratora kao String
+     */
     private String password;
 
+    /**
+     * Metoda koja poredi dva administratora po njihovim ID-u i vraca true ili false
+     * 
+     * @param obj Objekat sa kojim se vrsi uporedjivanje
+     * @return
+     * <ul>
+     * <li>true - ako su oba objekata klase Administrator i imaju isti ID.</li>
+     * <li>false - u svim ostalim slucajevima.</li>
+     * </ul>
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -31,9 +66,21 @@ public class Administrator extends AbstractDomainObject{
         return true;
     }
 
+    /**
+     * Konstruktor koji inicijalizuje objekat klase Administrator
+     */
     public Administrator() {
     }
 
+    /**
+     * Konstruktor koji inicijalizuje objekat klase Administrator sa prosledjenim vrednostima svih atributa
+     * 
+     * @param administratorID ID administratora kao Long
+     * @param ime Ime administratora kao String
+     * @param prezime Prezime administratora kao String
+     * @param username Username administratora kao String
+     * @param password Password administratora kao String 
+     */
     public Administrator(Long administratorID, String ime, String prezime, String username, String password) {
         this.administratorID = administratorID;
         this.ime = ime;
@@ -42,46 +89,121 @@ public class Administrator extends AbstractDomainObject{
         this.password = password;
     }
 
+    /**
+     * Metoda koja vraca ID administratora
+     * 
+     * @return ID administratora kao Long
+     */
     public Long getAdministratorID() {
         return administratorID;
     }
 
+    /**
+     * Metoda koja postavlja ID administratora
+     * 
+     * @param administratorID ID administratora kao Long
+     * @throws IllegalArgumentException ako je administratorID manji od 1
+     */
     public void setAdministratorID(Long administratorID) {
+    	if(administratorID < 1) {
+    		throw new IllegalArgumentException("AdministratorID ne sme biti manji od jedan");
+    	}
         this.administratorID = administratorID;
     }
 
+    /**
+     * Metoda koja vraca username administratora
+     * 
+     * @return Username administratora kao String
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Metoda koja postavlja username administratora
+     * 
+     * @param username Username administratora kao String 
+     * @throws NullPointerException ako je username null
+     */
     public void setUsername(String username) {
+    	if(username == null) {
+    		throw new NullPointerException("Username ne sme biti null");
+    	}
         this.username = username;
     }
 
+    /**
+     * Metoda koja vraca password administratora
+     * 
+     * @return password administratora kao String
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Metoda koja postavlja password administratora
+     * 
+     * @param password Password administratora kao String 
+     * @throws NullPointerException ako je password null
+     */
     public void setPassword(String password) {
+    	if(password == null) {
+    		throw new NullPointerException("Password ne sme biti null");
+    	}
         this.password = password;
     }
 
+    /**
+     * Metoda koja vraca ime administratora
+     * 
+     * @return ime administratora kao String
+     */
     public String getIme() {
         return ime;
     }
 
+    /**
+     * Metoda koja postavlja ime administratora
+     * 
+     * @param ime Ime administratora kao String 
+     * @throws NullPointerException ako je ime null
+     */
     public void setIme(String ime) {
+    	if(ime == null) {
+    		throw new NullPointerException("Ime ne sme biti null");
+    	}
         this.ime = ime;
     }
 
+    /**
+     * Metoda koja vraca prezime administratora
+     * 
+     * @return prezime administratora kao String
+     */
     public String getPrezime() {
         return prezime;
     }
 
+    /**
+     * Metoda koja postavlja prezime administratora
+     * 
+     * @param prezime Prezime administratora kao String 
+     * @throws NullPointerException ako je prezime null
+     */
     public void setPrezime(String prezime) {
+    	if(prezime == null) {
+    		throw new NullPointerException("Prezime ne sme biti null");
+    	}
         this.prezime = prezime;
     }
 
+    /**
+     * Metoda koja vraca String koji predstavlja ime i prezime administratora
+     * 
+     * @return Ime i prezime administratora kao String 
+     */
     @Override
     public String toString() {
         return ime + " " + prezime;
