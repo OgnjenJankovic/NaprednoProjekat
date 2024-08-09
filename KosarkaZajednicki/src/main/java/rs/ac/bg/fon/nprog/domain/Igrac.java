8a51dc30eb83704cb06216f7a7056ea290f50c81
OgnjenJankovic <oj20190129@student.fon.bg.ac.rs>
2024-08-09 14:06:58 +0200

    Dokumentovane klase Igrac i Korisnik

--- KosarkaZajednicki/src/main/java/rs/ac/bg/fon/nprog/domain/Igrac.java
+++ KosarkaZajednicki/src/main/java/rs/ac/bg/fon/nprog/domain/Igrac.java
@@ -4,13 +4,43 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+/**
+ * Klasa koja predstavlja igraca termina.
+ * 
+ * Ova klasa implementira klasu AbstractDomainObject u odnosu na tabelu igrac u bazi
+ * 
+ * Igrac ima ID termina koji je jedinstven identifikator,redni broj igraca, napomenu i korisnika
+ * 
+ * @author 2000o
+ * @version 1.1.0
+ */
 public class Igrac extends AbstractDomainObject{
 
+	/**
+     * Termin igraca kao objekat klase Termin
+     */
 	private Termin termin;
+	/**
+     * Redni broj igraca kao int
+     */
     private int rbIgraca;
+    /**
+     * Napomena za igraca kao String 
+     */
     private String napomena;
+    /**
+     * Korisnik igrac kao objekat klase Korisnik
+     */
     private Korisnik korisnikIgrac;
 
+    /**
+	 * Konstruktor koji inicijalizuje objekat klase Igrac sa prosledjenim vrednostima svih atributa.
+	 * 
+	 * @param termin termin igraca kao objekat klase Termin.
+	 * @param rbIgraca Redni broj igraca kao int.
+	 * @param napomena Napomena igraca kao String.
+	 * @param korisnik Korisnik kao objekat klase Korisnik.
+	 */
     public Igrac(Termin termin, int rbIgraca, String napomena, Korisnik korisnik) {
         this.termin = termin;
         this.rbIgraca = rbIgraca;
@@ -18,6 +48,9 @@ public class Igrac extends AbstractDomainObject{
         this.korisnikIgrac = korisnik;
     }
 
+    /**
+	 * Konstruktor koji inicijalizuje objekat klase Igrac
+	 */
     public Igrac() {
     }
 
@@ -109,10 +142,21 @@ public class Igrac extends AbstractDomainObject{
         return " WHERE TER.TERMINID = " + termin.getTerminID();
     }
 
+    /**
+	 * Metoda koja vraca termin igraca.
+	 * 
+	 * @return termin igraca kao objekat klase Termin.
+	 */
     public Termin getTermin() {
         return termin;
     }
 
+    /**
+  	 * Metoda koja postavlja termin igraca.
+  	 * 
+  	 * @param termin Termin igraca kao objekat klase Termin.
+  	 * @throws NullPointerException ako je termin null
+  	 */
     public void setTermin(Termin termin) {
     	if(termin==null) {
     		throw new NullPointerException("termin ne sme biti null");
@@ -120,10 +164,21 @@ public class Igrac extends AbstractDomainObject{
         this.termin = termin;
     }
 
+    /**
+	 * Metoda koja vraca redni broj igraca
+	 * 
+	 * @return rb igraca kao int
+	 */
     public int getRbIgraca() {
         return rbIgraca;
     }
 
+    /**
+	 * Metoda koja postavlja redni broj igraca.
+	 * 
+	 * @param rbIgraca Redni broj igraca kao int.
+	 * @throws IllegalArgumentException ako je korisnikID manji od 0
+	 */
     public void setRbIgraca(int rbIgraca) {
     	if(rbIgraca<0) {
     		throw new IllegalArgumentException("rbIgraca ne sme biti manji od nula");
@@ -131,10 +186,21 @@ public class Igrac extends AbstractDomainObject{
         this.rbIgraca = rbIgraca;
     }
 
+    /**
+	 * Metoda koja vraca napomenu igraca.
+	 * 
+	 * @return napomenu igraca kao String.
+	 */
     public String getNapomena() {
         return napomena;
     }
 
+    /**
+	 * Metoda koja postavlja napomenu igraca.
+	 * 
+	 * @param napomenu Napomenu igraca kao String.
+	 * @throws NullPointerException ako je napomenu null
+	 */
     public void setNapomena(String napomena) {
     	if(napomena==null) {
     		throw new NullPointerException("napomena ne sme biti null");
@@ -142,10 +208,21 @@ public class Igrac extends AbstractDomainObject{
         this.napomena = napomena;
     }
 
+    /**
+	 * Metoda koja vraca korisnika igraca.
+	 * 
+	 * @return korisnika igraca kao objekat klase Korisnik.
+	 */
     public Korisnik getKorisnikIgrac() {
         return korisnikIgrac;
     }
 
+    /**
+  	 * Metoda koja postavlja korisnika igraca.
+  	 * 
+  	 * @param korisnikIgrac Korisnik igraca kao objekat klase Korisnik.
+  	 * @throws NullPointerException ako je korisnikIgrac null
+  	 */
     public void setKorisnikIgrac(Korisnik korisnikIgrac) {
     	if(korisnikIgrac==null) {
     		throw new NullPointerException("korisnikIgrac ne sme biti null");
--- KosarkaZajednicki/src/main/java/rs/ac/bg/fon/nprog/domain/Korisnik.java
+++ KosarkaZajednicki/src/main/java/rs/ac/bg/fon/nprog/domain/Korisnik.java
@@ -4,20 +4,62 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+/**
+ * Klasa koja predstavlja korisnika termina.
+ * 
+ * Ova klasa implementira klasu AbstractDomainObject u odnosu na tabelu korisnik u bazi
+ * 
+ * Korisnik ima ID koji je jedinstven identifikator,ime i prezime, email, broj telefona i tipKorisnika
+ * 
+ * @author 2000o
+ * @version 1.1.0
+ */
 public class Korisnik extends AbstractDomainObject{
 
+	/**
+	 * ID korisnika kao Long
+	 */
 	private Long korisnikID;
+	/**
+	 * Ime korisnika kao String
+	 */
     private String ime;
+    /**
+     * Prezime korisnika kao String
+     */
     private String prezime;
+    /**
+     * Email korisnika kao String
+     */
     private String email;
+    /**
+     * Telefon korisnika kao String
+     */
     private String telefon;
+    /**
+     * Tip korisnika kao objekat klase Tip
+     */
     private Tip tipKorisnika;
 
+    /**
+	 * Vraca String koji predstavlja ime i prezime korisnika
+	 * @return Ime i prezime korisnika kao String.
+	 */
     @Override
     public String toString() {
         return ime + " " + prezime;
     }
 
+    /**
+	 * Konstruktor koji inicijalizuje objekat klase Korisnik sa prosledjenim vrednostima svih atributa.
+	 * 
+	 * @param korisnikID ID korisnika kao Long.
+	 * @param ime Ime korisnika kao String.
+	 * @param prezime Prezime korisnika kao String.
+	 * @param email Email korisnika kao String.
+	 * @param telefon Telefon korisnika kao String.
+	 * @param tipKorisnika Tip korisnika kao objekat klase Tip.
+	 */
 	public Korisnik(Long korisnikID, String ime, String prezime, String email, String telefon, Tip tipKorisnika) {
 		super();
 		this.korisnikID = korisnikID;
@@ -28,6 +70,9 @@ public class Korisnik extends AbstractDomainObject{
 		this.tipKorisnika = tipKorisnika;
 	}
 
+	/**
+	 * Konstruktor koji inicijalizuje objekat klase Korisnik
+	 */
 	public Korisnik() {
     }
 
@@ -93,10 +138,21 @@ public class Korisnik extends AbstractDomainObject{
         return "";
     }
 
+    /**
+	 * Metoda koja vraca ID korisnika
+	 * 
+	 * @return ID korisnika kao Long
+	 */
     public Long getKorisnikID() {
         return korisnikID;
     }
 
+    /**
+	 * Metoda koja postavlja ID korisnika.
+	 * 
+	 * @param korisnikID ID korisnika kao Long.
+	 * @throws IllegalArgumentException ako je korisnikID manji od 1
+	 */
     public void setKorisnikID(Long korisnikID) {
     	if(korisnikID<0) {
     		throw new IllegalArgumentException("korisnikID ne sme biti manji od nula");
@@ -104,10 +160,21 @@ public class Korisnik extends AbstractDomainObject{
         this.korisnikID = korisnikID;
     }
 
+    /**
+	 * Metoda koja vraca ime korisnika.
+	 * 
+	 * @return ime korisnika kao String.
+	 */
     public String getIme() {
         return ime;
     }
 
+    /**
+	 * Metoda koja postavlja ime korisnika.
+	 * 
+	 * @param ime Ime korisnika kao String.
+	 * @throws NullPointerException ako je ime null
+	 */
     public void setIme(String ime) {
     	if(ime==null) {
     		throw new NullPointerException("ime ne sme biti null");
@@ -115,10 +182,21 @@ public class Korisnik extends AbstractDomainObject{
         this.ime = ime;
     }
 
+    /**
+	 * Metoda koja vraca prezime korisnika.
+	 * 
+	 * @return prezime korisnika kao String.
+	 */
     public String getPrezime() {
         return prezime;
     }
 
+    /**
+   	 * Metoda koja postavlja prezime korisnika.
+   	 * 
+   	 * @param prezime Prezime korisnika kao String.
+   	 * @throws NullPointerException ako je prezime null
+   	 */
     public void setPrezime(String prezime) {
     	if(prezime==null) {
     		throw new NullPointerException("prezime ne sme biti null");
@@ -126,10 +204,21 @@ public class Korisnik extends AbstractDomainObject{
         this.prezime = prezime;
     }
 
+    /**
+	 * Metoda koja vraca email korisnika.
+	 * 
+	 * @return email korisnika kao String.
+	 */
     public String getEmail() {
         return email;
     }
 
+    /**
+   	 * Metoda koja postavlja email korisnika.
+   	 * 
+   	 * @param email Email korisnika kao String.
+   	 * @throws NullPointerException ako je email null
+   	 */
     public void setEmail(String email) {
     	if(email==null) {
     		throw new NullPointerException("email ne sme biti null");
@@ -137,10 +226,21 @@ public class Korisnik extends AbstractDomainObject{
         this.email = email;
     }
 
+    /**
+	 * Metoda koja vraca telefon korisnika.
+	 * 
+	 * @return telefon korisnika kao String.
+	 */
     public String getTelefon() {
         return telefon;
     }
 
+    /**
+   	 * Metoda koja postavlja email korisnika.
+   	 * 
+   	 * @param email Email korisnika kao String.
+   	 * @throws NullPointerException ako je email null
+   	 */
     public void setTelefon(String telefon) {
     	if(telefon==null) {
     		throw new NullPointerException("telefon ne sme biti null");
@@ -148,10 +248,21 @@ public class Korisnik extends AbstractDomainObject{
         this.telefon = telefon;
     }
     
+    /**
+	 * Metoda koja vraca tip korisnika.
+	 * 
+	 * @return tip korisnika kao objekat klase Tip.
+	 */
    public Tip getTipKorisnika() {
 	   return tipKorisnika;
    }
 
+   /**
+  	 * Metoda koja postavlja tip korisnika.
+  	 * 
+  	 * @param tipKorisnika Tip korisnika kao objekat klase Tip.
+  	 * @throws NullPointerException ako je tipKorisnika null
+  	 */
    public void setTipKorisnika(Tip tipKorisnika) {
 	   if(tipKorisnika==null) {
    		throw new NullPointerException("tipKorisnika ne sme biti null");
